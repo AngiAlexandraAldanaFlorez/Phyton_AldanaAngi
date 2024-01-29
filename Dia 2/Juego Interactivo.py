efc1d81780ef7aff4a9e5437e6dcfec7dd4f730b
Angi <angialexandraaldana@gmail.com>
2024-01-29 09:03:36 -0500

    holi

--- Dia 2/Juego Interactivo.py
+++ Dia 2/Juego Interactivo.py
@@ -5,16 +5,20 @@ a=int
 for i in range(1,11,1):
     while a != NumeroSecreto:
         for j in range(1,11,1):
-            print (j)
-        a= (input('Ingresa un numero, intento #',j+0))
-        if NumeroSecreto > a:
-                    print ('Numero es mayor que el numero que ingresaste')
-        if NumeroSecreto < a:
-                    print ('Numero es menor que el numero que ingresaste')
-        if i == 10:
-                    print('Han acabado tus intentos')
-        if a == NumeroSecreto:
-                    print ('¡Adivinaste!')
+            a = int(input('Ingresa un numero, intento #'))
+            if j == 10:
+                print('Han acabado tus intentos')
+                break
+            else: 
+                a == NumeroSecreto
+                print ('¡Adivinaste!')
+            print("Intento # ", j)
+            if NumeroSecreto > a:
+                print ('Numero es mayor que el numero que ingresaste')
+            elif NumeroSecreto < a:
+                print ('Numero es menor que el numero que ingresaste')
+            
+            
 
     
 
