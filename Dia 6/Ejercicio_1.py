0ca9cecfc0a5e35701a270cf51a540bb629142d6
AngiAlexandraAldanaFlorez <156430244+AngiAlexandraAldanaFlorez@users.noreply.github.com>
2024-02-01 14:25:20 -0500

    Update Ejercicio_1.py

--- Dia 6/Ejercicio_1.py
+++ Dia 6/Ejercicio_1.py
@@ -3,15 +3,21 @@
 #Y PRECIOS DE LA TABLA, DONDE SE LE PREGUNTE AL USUARIO POR UN PRODUCTO
 #Y LA CANTIDAD. AL FINALIZAR MOSTRAR EN LA CONSOLA EL PRECIO TOTAL. SI
 #EL PRODUCTO NO ESTA DEBE MOSTRAR UN MENSAJE INFORMANDO SOBRE ELLO
-
-diccionario = {'Frijoles-':4000,'Arroz-':2200,'Papa-':1800}
+print('''Bienvenido, Te ofrecemos los siguientes productos''')
+diccionario = {'Zapatos-':200000,'Camisas-':75000,'Pantalones':100000}
 
 for llave , valor in diccionario.items():
     print(llave,valor)
 
 
-a=int(input('Ingrese numero del producto que quiere'))
-print(a)
+a=str(input('Ingrese producto que quiere comprar '))
+b=int(input('Ingrese cantidad del producto '))
 
-
-
+if (a=="Zapatos" or a=="zapatos"):
+    print('Valor a pagar= ',200000*b)
+elif (a=="Camisas" or a=="camisas"):
+    print('Valor a pagar= ',75000*b)
+elif (a=="Pantalones"or a=="pantalones"):
+    print('Valor a pagar= ',100000*b)
+else:
+    print("No se encuentra el producto")
