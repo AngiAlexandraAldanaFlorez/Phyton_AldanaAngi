ca9d65dc92967d17f9938faba99166051b448c78
AngiAlexandraAldanaFlorez <156430244+AngiAlexandraAldanaFlorez@users.noreply.github.com>
2024-02-01 00:12:25 -0500

    sleep

---
+++ Dia 5/Ejercicio #2.py
@@ -0,0 +1,12 @@
+def altura ()
+b=int(input('Ingresa cuantas mesas quieres que alla'))
+for mesa in range (b):
+    a= int(input('typo de moneda'))
+    altura=int(input('ingresa altura de la pata de la mesa'))
+    for grosor in range (a):  
+        typecoins= int(input('ingresa medida'))
+        list[typecoins]
+        
+        
+
+    
