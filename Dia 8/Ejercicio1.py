7cde122ce57c635fabcdaf6911df1f781a49c942
angi <angialexandraaldana@gmail.com>
2024-02-07 09:21:34 -0500

    cambios

--- Dia 8/Ejercicio1.py
+++ Dia 8/Ejercicio1.py
@@ -1,7 +1,12 @@
 import json
 
-p=open('data.json')
-Diccionario=json.load(p)
+P=open('data.json')
+miJson=json.load(P)
+print(miJson)
 
 #1. Devuelve un listado con todos los pedidos que se han realizado. Los pedidos deben estar 
 #ordenados por la fecha de realización, mostrando en primer lugar los pedidos más recientes.
+
+x=str(input('ingrese'))
+miJson['nombre']=x
+print(miJson)
